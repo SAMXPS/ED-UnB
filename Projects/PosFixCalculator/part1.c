015d6f19618407b52ddcfe0944420fd0c543b70b
SAM_XPS <bsbcraftplays@gmail.com>
2019-10-27 16:26:25 -0300

    Implemeting project part 1 (with tester)

---
+++ Projects/PosFixCalculator/part1.c
@@ -0,0 +1,102 @@
+/*
+    
+    @author: Samuel James
+    https://github.com/SAMXPS
+    Universidade de Brasília, UnB
+*/
+#include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
+
+char ctx_open [] = {'(', '[', '{'};
+char ctx_close[] = {')', ']', '}'};
+char operators[] = {'+', '-', '*', '/', '^'};
+
+#define isPart(a, b) _isPart(a, b, sizeof(b))
+bool _isPart(char c, char* l, int s) {
+    int i;
+    for (i=0; i<s; i++)
+        if(l[i] == c) return true;
+    return false;
+}
+
+int idx(char c, char*l) {
+    int i;
+    for (i=0;; i++)
+        if(l[i] == c) return i;
+}
+
+bool isNumber(char c) {
+    return (c = (c - '0')) >= 0 && c < 10;
+}
+
+
+bool validate(const char* exp, int len) {
+    int p = 0, i;
+    bool type = 0; 
+    /* 
+    1 - operando
+    0 - operador
+    */
+
+    while (p<len) {
+        #define curr exp[p]
+
+        if (curr == ' ') {
+            p++;
+            continue;
+        }
+        if (!type) {
+            if (isNumber(curr)) {
+                while(p<len && isNumber(curr)) {
+                    p++;
+                }
+                type = 1;
+                continue;
+            }
+            else if (isPart(curr, ctx_open)) {
+                i = p;
+                while (1) {
+                    if (p == len) {
+                        return false;
+                    }
+                    if (isPart(curr, ctx_close)) {
+                        if (idx(curr, ctx_close) != idx(exp[i], ctx_open)) {
+                            /* Wrong context close */
+                            return false;
+                        }
+                        i++;
+                        if (!validate(exp + i, p-i)) return false;
+                        break;
+                    }
+                    p++;
+                }
+                type = 1;
+            } else return false;
+        } else if (isPart(curr, operators)) {
+            type = 0;
+            p++;
+            continue;
+        } else return false;
+        p++;
+    }
+    return type;
+}
+
+bool valida(const char* expressao) {
+    return validate(expressao, strlen(expressao));
+}
+
+int main() {
+    char expressao[101];a
+    
+    scanf("%100[^\n]", expressao);
+    
+    if (valida(expressao))
+        printf("VALIDA\n");
+    else
+        printf("INVALIDA\n");
+        
+    return 0;
+}
+
---
+++ Projects/PosFixCalculator/test_1.py
@@ -0,0 +1,41 @@
+import os
+import subprocess
+
+with subprocess.Popen('gcc -Wall -ansi part1.c -o part1.exe'.split(" "), stdout=subprocess.PIPE) as compa:
+    out = compa.stdout.read()
+    comp = out.decode("utf-8")
+
+valid = [
+    "1 + 2 * 3",
+    "[(1 + 2) * 3]",
+    "(1 + 2) ^ (3)",
+    "{2 ^ [3 - (4 / 8)]}",
+    "1.5 + 3.14 * (7)"
+]
+
+invalid = [
+    "(1 + 2) * 3)",
+    ")1 + 2( * (3)",
+    "2 2",
+    "1 + (2 * 3("
+]
+
+def test_me(v, exp):
+    with subprocess.Popen(["part1.exe"], stdout=subprocess.PIPE, stdin=subprocess.PIPE) as proc:
+        proc.stdin.write(bytes(v + "\n", 'ascii'))
+        proc.stdin.flush()
+        out = proc.stdout.read().decode('ascii')
+        if exp in out:
+            print("[PASS] " + v + ": " + out[:-2])
+        else:
+            print("[FAIL] " + v + ":" + out[:-2] + "-> (expected: " + exp + ")")
+
+
+if len(comp) > 0:
+    print("Test interrupted by compilation problems/warnings:")
+    print(comp)
+else:
+    for v in valid:
+        test_me(v, "VALIDA")
+    for v in invalid:
+        test_me(v, "INVALIDA")
