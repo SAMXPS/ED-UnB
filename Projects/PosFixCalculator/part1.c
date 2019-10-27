c27f6ce3fd78661d1fd9c79e010d250e0b129749
SAM_XPS <bsbcraftplays@gmail.com>
2019-10-27 19:03:29 -0300

    Finishing Project - Part 1

--- Projects/PosFixCalculator/part1.c
+++ Projects/PosFixCalculator/part1.c
@@ -27,12 +27,12 @@ int idx(char c, char*l) {
 }
 
 bool isNumber(char c) {
-    return (c = (c - '0')) >= 0 && c < 10;
+    return c == '.' || ((c = (c - '0')) >= 0 && c < 10);
 }
 
 
 bool validate(const char* exp, int len) {
-    int p = 0, i;
+    int p = 0, i, k;
     bool type = 0; 
     /* 
     1 - operando
@@ -55,19 +55,27 @@ bool validate(const char* exp, int len) {
                 continue;
             }
             else if (isPart(curr, ctx_open)) {
+                k = 0;
                 i = p;
+                p++;
                 while (1) {
                     if (p == len) {
                         return false;
                     }
+                    if (isPart(curr, ctx_open)) {
+                        k++;
+                    }
                     if (isPart(curr, ctx_close)) {
-                        if (idx(curr, ctx_close) != idx(exp[i], ctx_open)) {
-                            /* Wrong context close */
-                            return false;
+                        if (!k){
+                            if (idx(curr, ctx_close) != idx(exp[i], ctx_open)) {
+                                /* Wrong context close */
+                                return false;
+                            }
+                            i++;
+                            if (!validate(exp + i, p-i)) return false;
+                            break;
                         }
-                        i++;
-                        if (!validate(exp + i, p-i)) return false;
-                        break;
+                        k--;
                     }
                     p++;
                 }
